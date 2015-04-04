4ebedb62e7477d5f67fd2ee2fac541408ff3d903
Travis <travis@mcdem.us>
2015-04-04 13:49:54 -0400

    Move flag variables into scope of the main function
    The flags aren't used anywhere else, so keep them in the main scope.

--- echorequest.go
+++ echorequest.go
@@ -10,15 +10,13 @@ import (
 	"os"
 )
 
-var port string
-var url string
 
 // "Echo" the HTTP headers and payload of the HTTP request in the response.
 func main() {
-	flag.StringVar(&port, "port", ":8080", "The HTTP port to listen on.")
-	flag.StringVar(&url, "url", "/", "The URL path to serve.")
+	port := flag.String("port", ":8080", "The HTTP port to listen on.")
+	url := flag.String("url", "/", "The URL path to serve.")
 	flag.Parse()
-	http.HandleFunc(url, func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc(*url, func(w http.ResponseWriter, r *http.Request) {
 		m := io.MultiWriter(os.Stdout, w)
 		defer r.Body.Close()
 		b, err := ioutil.ReadAll(r.Body)
@@ -31,7 +29,7 @@ func main() {
 		}
 		fmt.Fprintf(m, string(b))
 	})
-	err := http.ListenAndServe(port, nil)
+	err := http.ListenAndServe(*port, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
